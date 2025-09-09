cc0531a3b560e340be73280df094696f072301e6
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2025-09-08 21:45:05 -0600

    customized flags for hook

--- script/00_DeployHook.s.sol
+++ script/00_DeployHook.s.sol
@@ -8,23 +8,23 @@ import {BaseScript} from "./base/BaseScript.sol";
 
 import {ReHypothecation} from "../src/ReHypothecation.sol";
 
-/// @notice Mines the address and deploys the ReHypothecation.sol Hook contract
+/// @notice Mines the address and deploys the Counter.sol Hook contract
 contract DeployHookScript is BaseScript {
     function run() public {
         // hook contracts must have specific flags encoded in the address
-        uint160 flags = uint160(
-            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
-        );
+        uint160 flags = uint160(Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG);
 
         // Mine a salt that will produce a hook address with the correct flags
         bytes memory constructorArgs = abi.encode(poolManager);
-        (address hookAddress, bytes32 salt) =
+
+        (address hookAddress, bytes32 _salt) =
             HookMiner.find(CREATE2_FACTORY, flags, type(ReHypothecation).creationCode, constructorArgs);
 
+        vm.label(address(hookAddress), "HookAddress");
         // Deploy the hook using CREATE2
         vm.startBroadcast();
-        ReHypothecation reHypothecation = new ReHypothecation{salt: salt}(poolManager);
+        ReHypothecation reHypothecation = new ReHypothecation{salt: _salt}(poolManager);
+        vm.label(address(reHypothecation), "ReHypothecation");
         vm.stopBroadcast();
 
         require(address(reHypothecation) == hookAddress, "DeployHookScript: Hook Address Mismatch");
